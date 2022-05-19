4283ae8d6a23328d048953acb9576aea7e658229
gustavo <gustavolzss12@gmail.com>
2022-05-19 17:16:42 -0300

    controller chamados

--- app/Http/Controllers/ChamadosController.php
+++ app/Http/Controllers/ChamadosController.php
@@ -14,7 +14,8 @@ class ChamadosController extends Controller
      */
     public function index()
     {
-        //
+        $chamados = Chamados::all();
+        return response()->json($chamados);
     }
 
     /**
@@ -25,30 +26,65 @@ class ChamadosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $newChamado = new Chamados([
+            'id_usuario' => $request->get('id_usuario'),
+            'id_atendente' => $request->get('id_atendente'),
+            'id_categoria' => $request->get('id_categoria'),
+            'id_chamado_pai' => $request->get('id_chamado_pai'),
+            'id_departamento' => $request->get('id_departamento'),
+            'titulo' => $request->get('titulo'),
+            'descricao' => $request->get('descricao'),
+            'impacto' => $request->get('impacto'),
+            'status' => $request->get('status')
+          ]);
+
+          $newChamado->save();
+
+          return response()->json($newChamado, 201);
+
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Chamados  $chamados
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Chamados $chamados)
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function show($id)
     {
-        //
+
+        $chamado = Chamados::findOrFail($id);
+
+        return response()->json($chamado);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Chamados  $chamados
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Chamados $chamados)
+    public function update(Request $request, $id)
     {
-        //
+
+        $chamado = Chamados::findOrFail($id);
+
+        $chamado->id_usuario = $request->get('id_usuario');
+        $chamado->id_atendente = $request->get('id_atendente');
+        $chamado->id_categoria = $request->get('id_categoria');
+        $chamado->id_chamado_pai = $request->get('id_chamado_pai');
+        $chamado->id_departamento = $request->get('id_departamento');
+        $chamado->titulo = $request->get('titulo');
+        $chamado->descricao = $request->get('descricao');
+        $chamado->impacto = $request->get('impacto');
+        $chamado->status = $request->get('status');
+
+
+        $chamado->save();
+
+        return response()->json($chamado,200);
     }
 
     /**
